8f74201e6b8b35192490630c88dd487480cc6b33
Alexander Wühr <wuehr@sc-networks.com>
2015-05-25 08:47:53 +0200

    Fix for failing php7 test

--- tests/J/ServerIntegrationTest.php
+++ tests/J/ServerIntegrationTest.php
@@ -30,7 +30,7 @@ class ServerIntegrationTest extends \PHPUnit_Framework_TestCase {
             ['{"jsonrpc": "2.0", "method": "subtract", "params": [42, 23], "id": 1}', '{"jsonrpc": "2.0", "result": 19, "id": 1}'],
             ['{"jsonrpc": "2.0", "method": "subtract", "params": [23, 42], "id": 1}', '{"jsonrpc": "2.0", "result": -19, "id": 1}'],
             ['{"jsonrpc": "2.0", "method": "subtract", "params": {"subtrahend": 23, "minuend": 42},"id": 3}', '{"jsonrpc": "2.0", "result": 19, "id": 3}'],
-            ['{"jsonrpc": "2.0", "method": "update", "params": [1,2,3,4,5]}', ''],
+            ['{"jsonrpc": "2.0", "method": "update", "params": [1,2,3,4,5]}', '""'],
             ['{"jsonrpc": "2.0", "method": "foobar", "id": "1"}', '{"jsonrpc": "2.0", "error": {"code": -32601, "message": "Method not found"}, "id": "1"}'],
             ['{"jsonrpc": "2.0", "method": "foobar, "params": "bar", "baz]', '{"jsonrpc": "2.0", "error": {"code": -32700, "message": "Parse error"}, "id": null}'],
             ['{"jsonrpc": "2.0", "method": 1, "params": "bar"}', '{"jsonrpc": "2.0", "error": {"code": -32600, "message": "Invalid Request"}, "id": null}'],
@@ -41,7 +41,7 @@ class ServerIntegrationTest extends \PHPUnit_Framework_TestCase {
                     '[{"jsonrpc": "2.0", "method": "sum", "params": [1,2,4], "id": "1"},{"jsonrpc": "2.0", "method": "notify_hello", "params": [7]},{"jsonrpc": "2.0", "method": "subtract", "params": [42,23], "id": "2"},{"foo": "boo"},{"jsonrpc": "2.0", "method": "foo.get", "params": {"name": "myself"}, "id": "5"},{"jsonrpc": "2.0", "method": "get_data", "id": "9"}]',
                     '[{"jsonrpc": "2.0", "result": 7, "id": "1"},{"jsonrpc": "2.0", "result": 19, "id": "2"},{"jsonrpc": "2.0", "error": {"code": -32600, "message": "Invalid Request"}, "id":null},{"jsonrpc": "2.0", "error": {"code": -32601, "message": "Method not found"}, "id": "5"},{"jsonrpc": "2.0", "result": ["hello", 5], "id": "9"}]'
             ],
-            ['[{"jsonrpc": "2.0", "method": "notify_sum", "params": [1,2,4]},{"jsonrpc": "2.0", "method": "notify_hello", "params": [7]}]', ''],
+            ['[{"jsonrpc": "2.0", "method": "notify_sum", "params": [1,2,4]},{"jsonrpc": "2.0", "method": "notify_hello", "params": [7]}]', '""'],
             ['"foo"', '{"id":null,"jsonrpc":"2.0","error":{"code":-32600,"message":"Invalid Request"}}'],
         ];
     }
